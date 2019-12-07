ebfabaaf7bd457207add6dc67b79b3e072c5e324
bijaysanjel <bjsanjel_1914@yahoo.com>
2019-12-07 15:37:03 -0800

    added pom and source file

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,30 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    private static WebDriver driver;
+
+    @BeforeSuite
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+    @Test
+    public void doLogin() throws InterruptedException {
+
+        driver.get("http://gmail.com");
+        driver.findElement(By.id("identifierId")).sendKeys("dfsvsv");
+
+    }
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+    }
+}
