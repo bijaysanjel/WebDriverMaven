a21f904a473a4ca05c14cb7212442306d134d5cb
bijaysanjel <bjsanjel_1914@yahoo.com>
2019-12-07 16:48:43 -0800

    mavengit: added comment

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -21,7 +21,7 @@ public class LoginTest {
 
         driver.get("http://gmail.com");
         driver.findElement(By.id("identifierId")).sendKeys("dfsvsv");
-        driver.findElement(By.xpath("//span[text()='Next']")).click();
+        driver.findElement(By.xpath("//span[text()='Next']")).click();//clicking next button
 
     }
     @AfterSuite
